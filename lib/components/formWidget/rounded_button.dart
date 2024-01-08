79bab7c4a80810b1fe503b41a96ac4c3a515bf11
tdev <ktgz14211771329@gmail.com>
2024-01-08 09:03:44 +0700

    sucess

--- lib/components/formWidget/rounded_button.dart
+++ lib/components/formWidget/rounded_button.dart
@@ -1,34 +1,44 @@
 import 'package:flutter/material.dart';
 
-class RoundedButton extends StatelessWidget {
+class RoundedButton extends StatefulWidget {
   const RoundedButton({Key? key, required this.label, required this.onPressed})
       : super(key: key);
 
   final String label;
-
   final VoidCallback onPressed;
+
+  @override
+  _RoundedButtonState createState() => _RoundedButtonState();
+}
+
+class _RoundedButtonState extends State<RoundedButton> {
+  bool isPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        widget.onPressed();
+        setState(() {
+          isPressed = !isPressed;
+        });
+      },
       style: ElevatedButton.styleFrom(
         padding: EdgeInsets.zero,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        backgroundColor: Colors.amber[500],
+        backgroundColor: isPressed ? Colors.amber[300] : Colors.amber[500],
       ),
       child: Ink(
         decoration: BoxDecoration(
-          color: Colors.amber[500],
           borderRadius: BorderRadius.circular(20),
         ),
         child: Container(
           height: 40,
           alignment: Alignment.center,
           child: Text(
-            label,
+            widget.label,
             style: const TextStyle(fontSize: 15, color: Colors.white),
           ),
         ),
